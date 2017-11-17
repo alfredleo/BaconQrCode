53e03123038ab7ae0ad6cfe2e31d2c49d00e4c31
Alfred <alfredleo@gmail.com>
2017-11-17 15:40:43 +0500

    Testing RGBA support

--- index.php
+++ index.php
@@ -8,16 +8,21 @@
 
 // for algorithms on pure gd with good antialias see http://create.stephan-brumme.com/antialiased-circle/
 
-require(__DIR__ . '/vendor/autoload.php');
+use BaconQrCode\Renderer\Color\Rgb;
+
+require(__DIR__ . '/autoload_register.php');
 include_once(__DIR__ . '/imageSmoothArc.php');
 //include_once('C:\wamp64\helpers\dumpphp\dumping.php');
 
 $start = microtime(true);
 $renderer = new \BaconQrCode\Renderer\Image\Png();
-$renderer->setHeight(800);
-$renderer->setWidth(800);
+$renderer->setHeight(500);
+$renderer->setWidth(500);
+$renderer->setMargin(1);
+$renderer->setBackgroundColor(new Rgb(255, 255, 255));
+$renderer->setForegroundColor(new Rgb(51, 49, 60));
 $writer = new \BaconQrCode\Writer($renderer);
-$text = 'http://www.sabgames.org';
+$text = 'http://aaaaaaaa.bbbbbb.org/api/promotion/getPromotionInfo?id=36';
 if (isset($_REQUEST["text"]) && (strlen($_REQUEST["text"]) > 0))
     $text = $_REQUEST["text"];
 
